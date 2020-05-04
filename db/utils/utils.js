14377400b7385e23de81b3e58df555854723617d
BitterBlue22 <telisax@gmail.com>
2020-05-04 22:21:35 +0100

    utils formatDates passes all tests

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,9 @@
-exports.formatDates = list => {};
-
-exports.makeRefObj = list => {};
-
-exports.formatComments = (comments, articleRef) => {};
+exports.formatDates = (list) => {
+  const newList = [];
+  list.forEach((article) => {
+    let newObj = { ...article };
+    newObj.created_at = new Date(article.created_at);
+    newList.push(newObj);
+  });
+  return newList;
+};
