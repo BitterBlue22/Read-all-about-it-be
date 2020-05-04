fddf6a54d368a7b051dfb93ea259de69ce2b5e56
BitterBlue22 <telisax@gmail.com>
2020-05-04 22:05:43 +0100

    test set for formatDates utilis

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -2,10 +2,23 @@ const {
   formatDates,
   makeRefObj,
   formatComments,
-} = require('../db/utils/utils');
+} = require("../db/utils/utils");
+const { articleData } = require("../db/data/test-data/index");
 
-describe('formatDates', () => {});
-
-describe('makeRefObj', () => {});
-
-describe('formatComments', () => {});
+describe("formatDates", () => {
+  test("returns an array when passed one", () => {
+    expect(formatDates([])).toEqual([]);
+  });
+  test("returns an array with formatted dates", () => {
+    const date = formatDates(articleData);
+    expect(date[0].created_at).toEqual(new Date(1542284514171));
+  });
+  test("checks that array has not mutated the original", () => {
+    formatDates(articleData);
+    expect(articleData).toEqual(articleData);
+  });
+  test("checks that the returned array is not the original", () => {
+    const output = formatDates(articleData);
+    expect(output[0]).not.toBe(articleData[0]);
+  });
+});
