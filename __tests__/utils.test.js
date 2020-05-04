2b98bd56dd0b36c19e56805ff6d1920d3c499e05
BitterBlue22 <telisax@gmail.com>
2020-05-04 22:32:33 +0100

    test set for makeRefObj utils

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -22,3 +22,50 @@ describe("formatDates", () => {
     expect(output[0]).not.toBe(articleData[0]);
   });
 });
+
+describe("makeRefObj", () => {
+  test("returns an object when passed an array", () => {
+    expect(makeRefObj([{}])).toEqual({});
+  });
+  test("returns a destructured object when keys and values are entered", () => {
+    expect(
+      makeRefObj([{ title: "nonesense", article_id: 1 }], "article_id", "title")
+    ).toEqual({ 1: "nonesense" });
+  });
+  test("works for an array of objects", () => {
+    const input = [
+      { title: "lorem100", article_id: 1 },
+      { title: "lorem80", article_id: 2 },
+      { title: "lorem60", article_id: 3 },
+      { title: "lorem40", article_id: 4 },
+    ];
+    const output = { 1: "lorem100", 2: "lorem80", 3: "lorem60", 4: "lorem40" };
+    expect(makeRefObj(input)).toEqual(output);
+  });
+  test("return object does not share the same mem ref as original", () => {
+    const input = [
+      { title: "lorem100", article_id: 1 },
+      { title: "lorem80", article_id: 2 },
+      { title: "lorem60", article_id: 3 },
+      { title: "lorem40", article_id: 4 },
+    ];
+    const result = makeRefObj(input);
+    expect(result).not.toBe(input);
+  });
+  test("check does not mutate original array", () => {
+    const input = [
+      { title: "lorem100", article_id: 1 },
+      { title: "lorem80", article_id: 2 },
+      { title: "lorem60", article_id: 3 },
+      { title: "lorem40", article_id: 4 },
+    ];
+    const control = [
+      { title: "lorem100", article_id: 1 },
+      { title: "lorem80", article_id: 2 },
+      { title: "lorem60", article_id: 3 },
+      { title: "lorem40", article_id: 4 },
+    ];
+    makeRefObj(input);
+    expect(input).toEqual(control);
+  });
+});
