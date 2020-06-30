2f12fe7cf81742fea23614a35ef967d5c57a4859
BitterBlue22 <telisax@gmail.com>
2020-06-30 20:35:31 +0100

    updates api router to add endpoints file

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -3,6 +3,8 @@ const articlesRouter = require("./articlesRouter");
 const topicsRouter = require("./topicsRouter");
 const usersRouter = require("./usersRouter");
 const commentsRouter = require("./commentsRouter");
+const endpoints = require("../endpoints.json");
+const { handle405s } = require("../controllers/errorHandler");
 
 apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/topics", topicsRouter);
@@ -10,7 +12,7 @@ apiRouter.use("/users", usersRouter);
 apiRouter.use("/comments", commentsRouter);
 
 apiRouter.get("/", (req, res, next) => {
-  res.status(200).send({ msg: "responding" });
+  res.status(200).send({ endpoints }).all(handle405s);
 });
 
 module.exports = apiRouter;
